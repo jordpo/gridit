1b6ffba8affb27d85f3ede2961c3498c2ce17a89
Jordan Morano <jordan.morano@gmail.com>
2014-03-24 08:39:43 -0400

    Initialize app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GridIt::Application.config.secret_key_base = '8b630576511f2a1957ea95f56737161252b13eba6b89c155d4384a401553403139b2febc87862925e0099684ce58266a38116a0f7882d7e608117d8988e9e5a5'
