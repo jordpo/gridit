ad041b126177dc193b85bb4531594602188782ed
Jordan Morano <jordan.morano@gmail.com>
2014-03-25 10:04:46 -0400

    add an additional helper

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -8,4 +8,14 @@ module DashboardHelper
     end
     return true
   end
+
+  def no_current_actual?(bills)
+    bills.each do |bill|
+      if bill.bill_period.month == Date.today.month - 1 &&
+        !bill.prediction
+        return false
+      end
+    end
+    return true
+  end
 end
