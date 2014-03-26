2d0c4344c0377b7ad619f84fdcdb8e121c6fdcc4
Jordan Morano <jordan.morano@gmail.com>
2014-03-26 11:23:49 -0400

    Add styling and a welcome msg for new visitor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
-
   # Update strong params in devise for new user fields
   before_action :update_sanitized_params, if: :devise_controller?
 
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class DashboardController < ApplicationController
   def index
-    bills = current_user.bills.order(bill_period: :desc)
-    @electric = bills.select { |x| x.utility == 'electric' }
-    @gas = bills.select { |x| x.utility == 'gas' }
+    if user_signed_in?
+      bills = current_user.bills.order(bill_period: :desc)
+      @electric = bills.select { |x| x.utility == 'electric' }
+      @gas = bills.select { |x| x.utility == 'gas' }
+    end
   end
 end
